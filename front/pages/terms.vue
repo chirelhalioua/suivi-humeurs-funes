4cba7a064d5f7920d58a9ee6237c48ce8dbcd2f5
ChirelHalioua <72791846+chirelhalioua@users.noreply.github.com>
2025-03-03 22:12:46 +0100

    Create terms.vue

---
+++ front/pages/terms.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nuxt-content :document="terms" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const terms = await $content('terms').fetch()
+    return { terms }
+  }
+}
+</script>
+
+<style>
+/* Ajoutez votre style ici si nécessaire */
+</style>
