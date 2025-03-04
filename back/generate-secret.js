d9d75863de17776fd3601a84f02ac661cd1aa76b
ChirelHalioua <chirelhalioua@gmail.com>
2025-03-04 20:10:37 +0100

    Fix JWT token and add userId

--- back/generate-secret.js
+++ back/generate-secret.js
@@ -1,7 +1,16 @@
-const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
-// Générer une clé secrète aléatoire
-const secretKey = crypto.randomBytes(64).toString('hex');
+// Créer un payload (données à encoder dans le token)
+const payload = {
+  userId: '67c7445b1283e3ae024a5800',  // L'ID utilisateur
+  role: 'admin',     // Le rôle de l'utilisateur
+};
 
-// Afficher la clé générée dans la console
-console.log(secretKey);
+// Créer un secret (assurez-vous de garder ce secret secret)
+const secretKey = '67d450cc93cb72dabb8da91b998b420ecb42649c453a0dfd5ba00a9de1f1218af1b8666667da1139e046d0f29b3dd498ff31ea0a02d0d1b336c776c4a9780bff';
+
+// Générer un token avec une expiration (par exemple, 1 heure)
+const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+
+console.log('Voici votre token JWT :');
+console.log(token);
