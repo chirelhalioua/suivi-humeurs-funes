4504a6a32e0d770c1128bde9004e07cbca496e21
ChirelHalioua <72791846+chirelhalioua@users.noreply.github.com>
2025-03-08 23:36:20 +0100

    Update authRoutes.js

--- back/routes/authRoutes.js
+++ back/routes/authRoutes.js
@@ -23,29 +23,9 @@ router.post('/login', loginUser);
 router.get('/users', getAllUsers);
 
 // Récupérer le profil de l'utilisateur (sans token, par userId dans la query)
-router.get('/profil', (req, res) => {
-  const userId = req.query.userId; // Récupérer l'userId depuis les paramètres de la requête
-
-  if (!userId) {
-    return res.status(400).json({ message: 'User ID is required' });
-  }
-
-  // Logique pour récupérer le profil de l'utilisateur depuis la base de données
-  User.findById(userId)
-    .then(user => {
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.json({ user });
-    })
-    .catch(err => res.status(500).json({ message: 'Server error' }));
-});
-
-// Récupérer le profil d'un utilisateur
 router.get('/profil', getUserProfile);
 
 // Supprimer un profil utilisateur
 router.delete('/profil', deleteUserProfile);
-
 
 module.exports = router;
