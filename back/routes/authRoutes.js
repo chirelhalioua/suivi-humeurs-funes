5bdb0f10299e4ba7dc28a8edc0302e686756ba52
ChirelHalioua <72791846+chirelhalioua@users.noreply.github.com>
2025-03-08 23:04:48 +0100

    Update authRoutes.js

--- back/routes/authRoutes.js
+++ back/routes/authRoutes.js
@@ -22,19 +22,8 @@ router.post('/login', loginUser);
 // Récupérer tous les utilisateurs
 router.get('/users', getAllUsers);
 
-// Récupérer le profil de l'utilisateur connecté (protégé)
-router.get('/profil', authMiddleware, async (req, res) => {
-  try {
-    const user = await User.findById(req.user._id);
-    if (!user) return res.status(404).send('Utilisateur non trouvé.');
-    res.json(user);
-  } catch (error) {
-    res.status(500).send('Erreur serveur.');
-  }
-});
-
-// Récupérer un profil via son ID
-router.get('/profil/:userId', getUserProfile);
+// Récupérer le profil de l'utilisateur connecté (route protégée)
+router.get('/profil', authMiddleware, getUserProfile);
 
 // Suppression du profil de l'utilisateur connecté (protégé)
 router.delete('/profil', authMiddleware, deleteUserProfile);
