e59c5f22ff5178f6abf07b44bdf721eda53c20a9
ChirelHalioua <72791846+chirelhalioua@users.noreply.github.com>
2026-09-16 17:38:26 +0200

    Autorise les photos de profil redimensionnées

--- back/server.js
+++ back/server.js
@@ -37,8 +37,8 @@ const corsOptions = {
 app.use(cors(corsOptions)); // Appliquer CORS
 
 // Middleware pour analyser les requêtes
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json({ limit: "1mb" })); 
+app.use(express.urlencoded({ extended: true, limit: "1mb" })); 
 
 app.options("*", cors(corsOptions));
 
