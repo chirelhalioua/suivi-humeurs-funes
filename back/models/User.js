68f8e1ee64fc2afb824dec8800b0ed58f0db51da
ChirelHalioua <72791846+chirelhalioua@users.noreply.github.com>
2026-09-16 17:38:11 +0200

    Ajoute la photo de profil utilisateur

--- back/models/User.js
+++ back/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  profileImage: { type: String, default: '' },
   resetPasswordToken: { type: String, default: null },
   resetPasswordExpires: { type: Date, default: null }
 });
